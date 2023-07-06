6222dc6e0ebb3455bb5d011d205ed7c47c6d467b
shubham <shubhamdixit863@gmail.com>
2023-07-06 10:03:16 +0530

    Add request

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,14 +1,45 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"simplegoapi/internal/dto"
+	"simplegoapi/internal/service"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+type Handler struct {
+	Svc service.Service
+}
+
+func (hn Handler) HomeHandler(w http.ResponseWriter, r *http.Request) {
 
 	// We can send response from here ----
 
-	fmt.Fprintf(w, "Hello world This is my first Route")
+}
+
+func (hn Handler) AddData(w http.ResponseWriter, r *http.Request) {
+	var user dto.AddRequest
+
+	// we will parse the data coming from request into our struct object
+
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		fmt.Fprintf(w, "Please send proper data")
+	}
+	fmt.Println(user)
+
+	hn.Svc.AddData(user)
+
+	fmt.Fprintf(w, "Request Received")
+
+}
+
+func (hn Handler) GetData(w http.ResponseWriter, r *http.Request) {
+
+	users := hn.Svc.GetData()
+	fmt.Println(users)
+
+	fmt.Fprintf(w, "Data")
 
 }
